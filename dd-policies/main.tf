74be286ec7a5d6323572612a6f5d3ba6bcb5ea50
yafernandes <alexandrino.fernandes@gmail.com>
2019-06-17 14:16:07 -0400

    Bug fix

--- dd-policies/main.tf
+++ dd-policies/main.tf
@@ -14,15 +14,20 @@ resource "aws_iam_role" "dd_role" {
   }
 }
 
-resource "aws_iam_role_policy" "dd_lambda" {
+resource "aws_iam_policy" "dd_lambda" {
   name   = "alexf_lambda"
-  role   = "${aws_iam_role.dd_role.id}"
   policy = "${file("policy-lambda.json")}"
 }
-
-resource "aws_iam_role_policy" "dd_aws" {
-  name   = "alexf_lambda"
-  role   = "${aws_iam_role.dd_role.id}"
+resource "aws_iam_role_policy_attachment" "dd_lambda" {
+  role       = "${aws_iam_role.dd_role.name}"
+  policy_arn = "${aws_iam_policy.dd_lambda.arn}"
+}
+resource "aws_iam_policy" "dd_aws" {
+  name   = "alexf_aws"
   policy = "${file("policy-aws.json")}"
+}
+resource "aws_iam_role_policy_attachment" "dd_aws" {
+  role       = "${aws_iam_role.dd_role.name}"
+  policy_arn = "${aws_iam_policy.dd_aws.arn}"
 }
 
